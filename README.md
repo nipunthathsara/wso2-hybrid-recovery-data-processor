6f478c957b8f3e2396437880639f522f86b00b52
nipunthathsara <nipunthathsara@gmail.com>
2019-12-07 08:01:08 -0300

    working status

--- README.md
+++ README.md
@@ -1,51 +1,28 @@
-## Custom Carbon Log Appender.
+## Hybrid recovery data persistence processor for both JDBC and Registry
 ### Introduction / Use case.
-This is a custom designed log appender that can be used to intercept logs in WSO2 Identity Server. The exact use case is as follows. 
+This is a hybrid persistence processor to store confirmation codes in either Registry
+or in the JDBC stores as configured. When loading the persisted codes for validation purposes, 
+this will check in both the places. But the priority will be given as configured.
 
-The focus of this particular example is to replace CRLF characters in carbon logs. **C**arriage **R**eturn (ASCII 13, \r) **L**ine **F**eed (ASCII 10, \n) characters in logs represent new lines and therefore can be used to change the logs or add fake entries by attackers. For example, see the below log entry in the code and the output they produce.
-
-Code
-
-```
-log.info("TEST LOG =============");
-log.info("TEST LOG \n some string \r some other string \n John Doe");
-log.info("TEST LOG =============");
-```
-
-Output
-
-```
-[2018-09-04 20:39:00,661]  INFO {org.wso2.carbon.user.core.ldap.ReadOnlyLDAPUserStoreManager} -  TEST LOG =============
-[2018-09-04 20:39:00,663]  INFO {org.wso2.carbon.user.core.ldap.ReadOnlyLDAPUserStoreManager} -  TEST LOG 
- some string
- some other string 
- John Doe
-[2018-09-04 20:39:00,663]  INFO {org.wso2.carbon.user.core.ldap.ReadOnlyLDAPUserStoreManager} -  TEST LOG =============
-```
-
-
-This is a popular security vulnerability known as [CRLF Injection](https://www.owasp.org/index.php/CRLF_Injection). Our simple solution to this problem is to is intercept all logs before appending to the log file and replace CRLF characters with a special character. Also, we're adding an additional text, **(Sanitized)** to the end of such entries so that we can find those easily for audit purposes.
+This will provide an smoother migration from Registry to JDBC without loosing any
+codes and vice versa. 
 
 ### Applicable product versions.
-Originally designed for WSO2 Identity Server 5.1.0
+WSO2 Identity Server 5.7.0
 
 ### How to use.
 Follow below steps to use this log appender.
-1. Build the project.
-2. Copy the JAR file into **<IS_HOME>/repository/components/dropins** directory.
-3. Open the file **<IS_HOME>/repository/conf/log4j.properties** file in a text editor and change the line,
-  ```log4j.appender.CARBON_LOGFILE=org.wso2.carbon.utils.logging.appenders.CarbonDailyRollingFileAppender```
-  to
-  ```log4j.appender.CARBON_LOGFILE=org.wso2.carbon.custom.utils.logging.appenders.CarbonDailyRollingSanitizedFileAppender```
-4. Restart the server. 
+1. Build the project. ```mvn clean install```
+2. Copy the JAR file into **<IS_HOME>/repository/components/lib** directory.
+3. Open the file **<IS_HOME>/repository/conf/identity/identity-mgt.properties** file in a text editor and change
+the recovery data processor's value as below.
+```Identity.Mgt.User.Recovery.Data.Store=org.wso2.carbon.identity.extended.recovery.store.HybridUserRecoveryDataStore```
+4. Add the below line to the same file.
+```Identity.Mgt.User.Recovery.Data.Store.isJDBCPrioritized=true```
+(This will give priority to JDBC when storing/loading the confirmation codes. If you prefer to have the Registry instead,
+please leave this ``false``)
+5. Restart the server.
 
-### Testing the project.
-We can add above test log entry to any suitable module and patch it to see the output. I added above test log to the constructor of [ReadOnlyLDAPUserStoreManager](https://github.com/wso2/carbon-kernel/blob/v4.4.3/core/org.wso2.carbon.user.core/src/main/java/org/wso2/carbon/user/core/ldap/ReadOnlyLDAPUserStoreManager.java#L131) file accordingly. 
-
-Once the server is restarted, you can see the modified logs are appending into **<IS_HOME>/repository/logs/wso2carbon.log** file. Modified log for above log entry is as follows.
-
-```
-TID: [-1234] [] [2018-09-04 20:39:00,661]  INFO {org.wso2.carbon.user.core.ldap.ReadOnlyLDAPUserStoreManager} -  TEST LOG ============= 
-TID: [-1234] [] [2018-09-04 20:39:00,663]  INFO {org.wso2.carbon.user.core.ldap.ReadOnlyLDAPUserStoreManager} -  TEST LOG _ some string _ some other string _ John Doe (Sanitized) 
-TID: [-1234] [] [2018-09-04 20:39:00,663]  INFO {org.wso2.carbon.user.core.ldap.ReadOnlyLDAPUserStoreManager} -  TEST LOG ============= 
-```
+### Persistence locations
+1. Registry - ```/_system/config/repository/components/org.wso2.carbon.identity.mgt/data``` registry path.
+2. JDBC - ```IDN_IDENTITY_META_DATA``` table.
--- src/main/java/org/wso2/carbon/identity/extended/recovery/store/HybridUserRecoveryDataStore.java
+++ src/main/java/org/wso2/carbon/identity/extended/recovery/store/HybridUserRecoveryDataStore.java
@@ -1,5 +1,7 @@
 package org.wso2.carbon.identity.extended.recovery.store;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.base.IdentityException;
 import org.wso2.carbon.identity.mgt.IdentityMgtConfig;
 import org.wso2.carbon.identity.mgt.dto.UserRecoveryDataDO;
@@ -9,16 +11,19 @@ import org.wso2.carbon.identity.mgt.store.UserRecoveryDataStore;
 
 public class HybridUserRecoveryDataStore implements UserRecoveryDataStore {
 
-    private boolean isJDBCPrioritise = false;
+    private static Log log = LogFactory.getLog(HybridUserRecoveryDataStore.class);
+
     private RegistryRecoveryDataStore registryRecoveryDataStore = new RegistryRecoveryDataStore();
     private JDBCUserRecoveryDataStore jdbcUserRecoveryDataStore = new JDBCUserRecoveryDataStore();
 
     @Override
     public void store(UserRecoveryDataDO recoveryDataDO) throws IdentityException {
 
-        if (isJDBCPrioritise) {
+        if (isJDBCPrioritized()) {
+            log.debug("Storing JDBC recovery data for the user : " + recoveryDataDO.getUserName());
             jdbcUserRecoveryDataStore.store(recoveryDataDO);
         } else {
+            log.debug("Storing Registry recovery data for the user : " + recoveryDataDO.getUserName());
             registryRecoveryDataStore.store(recoveryDataDO);
         }
     }
@@ -26,9 +31,11 @@ public class HybridUserRecoveryDataStore implements UserRecoveryDataStore {
     @Override
     public void store(UserRecoveryDataDO[] recoveryDataDOs) throws IdentityException {
 
-        if (isJDBCPrioritise) {
+        if (isJDBCPrioritized()) {
+            log.debug("Storing recovery data in the JDBC store.");
             jdbcUserRecoveryDataStore.store(recoveryDataDOs);
         } else {
+            log.debug("Storing recovery data in the Registry store.");
             registryRecoveryDataStore.store(recoveryDataDOs);
         }
     }
@@ -36,15 +43,19 @@ public class HybridUserRecoveryDataStore implements UserRecoveryDataStore {
     @Override
     public UserRecoveryDataDO load(String code) throws IdentityException {
 
-        if (isJDBCPrioritise) {
+        if (isJDBCPrioritized()) {
+            log.debug("Loading confirmation code from the JDBC first.");
             UserRecoveryDataDO userRecoveryDataDO = jdbcUserRecoveryDataStore.load(code);
             if (userRecoveryDataDO == null) {
+                log.debug("Loading confirmation code from the Registry, as the JDBC failed.");
                 userRecoveryDataDO = registryRecoveryDataStore.load(code);
             }
             return userRecoveryDataDO;
         } else {
+            log.debug("Loading confirmation code from the Registry first.");
             UserRecoveryDataDO userRecoveryDataDO = registryRecoveryDataStore.load(code);
             if (userRecoveryDataDO == null) {
+                log.debug("Loading confirmation code from the JDBC, as the Registry failed.");
                 userRecoveryDataDO = jdbcUserRecoveryDataStore.load(code);
             }
             return userRecoveryDataDO;
@@ -54,9 +65,11 @@ public class HybridUserRecoveryDataStore implements UserRecoveryDataStore {
     @Override
     public void invalidate(String code) throws IdentityException {
 
-        if (isJDBCPrioritise) {
+        if (isJDBCPrioritized()) {
+            log.debug("Invalidating code from the JDBC store.");
             jdbcUserRecoveryDataStore.invalidate(code);
         } else {
+            log.debug("Invalidating code from the Registry store.");
             registryRecoveryDataStore.invalidate(code);
         }
     }
@@ -64,9 +77,15 @@ public class HybridUserRecoveryDataStore implements UserRecoveryDataStore {
     @Override
     public void invalidate(UserRecoveryDataDO recoveryDataDO) throws IdentityException {
 
-        if (isJDBCPrioritise) {
+        if (isJDBCPrioritized()) {
+            if (recoveryDataDO != null) {
+                log.debug("Invalidating recovery data from the JDBC store. username : " + recoveryDataDO.getUserName());
+            }
             jdbcUserRecoveryDataStore.invalidate(recoveryDataDO);
         } else {
+            if (recoveryDataDO != null) {
+                log.debug("Invalidating recovery data from the Registry store. username : " + recoveryDataDO.getUserName());
+            }
             registryRecoveryDataStore.invalidate(recoveryDataDO);
         }
     }
@@ -74,9 +93,13 @@ public class HybridUserRecoveryDataStore implements UserRecoveryDataStore {
     @Override
     public void invalidate(String userId, int tenantId) throws IdentityException {
 
-        if (isJDBCPrioritise) {
+        if (isJDBCPrioritized()) {
+            log.debug("Invalidating recovery data for the userId : " + userId + ", tenantId : " + tenantId +
+                    " from the JDBC store.");
             jdbcUserRecoveryDataStore.invalidate(userId, tenantId);
         } else {
+            log.debug("Invalidating recovery data for the userId : " + userId + ", tenantId : " + tenantId +
+                    " from the Registry store.");
             registryRecoveryDataStore.invalidate(userId, tenantId);
         }
     }
@@ -84,10 +107,18 @@ public class HybridUserRecoveryDataStore implements UserRecoveryDataStore {
     @Override
     public UserRecoveryDataDO[] load(String userName, int tenantId) throws IdentityException {
 
-        if (isJDBCPrioritise) {
+        if (isJDBCPrioritized()) {
+            log.debug("Loading recovery data for the username : " + userName + ", tenantId : " + tenantId +
+                    " from the JDBC store.");
             return jdbcUserRecoveryDataStore.load(userName, tenantId);
         } else {
+            log.debug("Loading recovery data for the username : " + userName + ", tenantId : " + tenantId +
+                    " from the Registry store.");
             return registryRecoveryDataStore.load(userName, tenantId);
         }
+    }
+
+    private boolean isJDBCPrioritized() {
+        return Boolean.parseBoolean(IdentityMgtConfig.getInstance().getProperty(Constants.IS_JDBC_PRIORITIZED));
     }
 }
